680f824c155ed652d14bbaaa19f728c7a94748bc
xackermonkeay <toxa_s000@mail.ru>
2022-05-30 16:32:34 +0300

    v0.11

--- domain.js
+++ domain.js
@@ -1,5 +1,5 @@
-export const domain = "http://172.20.10.2:8000/mobile";
-export const domain_domain = "http://172.20.10.2:8000";
+// export const domain = "http://172.20.10.2:8000/mobile";
+// export const domain_domain = "http://172.20.10.2:8000";
 
-// export const domain = "https://devbag.bag24.ru/mobile";
-// export const domain_domain = "https://devbag.bag24.ru";
+export const domain = "https://devbag.bag24.ru/mobile";
+export const domain_domain = "https://devbag.bag24.ru";
